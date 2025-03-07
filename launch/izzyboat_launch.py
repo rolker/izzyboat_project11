1b5d6124e8aa9ef4fcc6638d2ce67a2220485f25
Roland Arsenault <roland@ccom.unh.edu>
2025-03-07 12:01:57 -0500

    Initial ROS2 port

---
+++ launch/izzyboat_launch.py
@@ -0,0 +1,195 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import TextSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    namespace = LaunchConfiguration('namespace')
+
+    namespace_arg = DeclareLaunchArgument(
+      "namespace", default_value=TextSubstitution(text="izzy")
+    )
+
+
+    fcu_url = LaunchConfiguration('fcu_url')
+    gcs_url = LaunchConfiguration('gcs_url')
+
+    fcu_url_arg = DeclareLaunchArgument(
+      "fcu_url", default_value=TextSubstitution(text="/dev/ttyACM0:57600")
+    )
+
+    gcs_url_arg = DeclareLaunchArgument(
+      "gcu_url", default_value=TextSubstitution(text="")
+    )
+
+
+    remappings = [('/tf', 'tf'), ('/tf_static', 'tf_static')]
+
+
+
+    return LaunchDescription(
+      namespace_arg,
+      fcu_url_arg,
+      gcs_url_arg,
+      IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+          PathJoinSubstitution([
+            FindPackageShare('echoboat_project11'),
+            'launch',
+            'echo_launch.py'
+          ])
+        ),
+        launch_arguments={
+          'namespace': namespace,
+          'fcu_url': fcu_url,
+          'gcs_url': gcs_url,
+        }.items()
+      ),
+
+    )
+
+
+
+
+# <launch>
+#   <arg name="namespace" default="izzy"/>
+#   <arg name="platform_name" default="$(arg namespace)"/>
+#   <arg name="platform_package" default="izzyboat_project11"/>
+#   <arg name="operator_host" default="salmonib"/>
+#   <arg name="logDirectory" default="/dev/null"/>
+#   <arg name="urdf_model" default="$(find izzyboat_project11)/urdf/izzyboat.urdf"/>
+#   <arg name="fcu_url" default="/dev/ttyUSB0:57600"/> 
+#   <arg name="gcs_url" default="udp://@192.168.12.8"/> 
+#   <!-- <arg name="gcs_url" default="udp://@192.168.13.142"/> -->
+
+
+#   <remap from="/$(arg namespace)/sensors/mbes/soundings" to="/$(arg namespace)/sensors/deltat/soundings"/>
+#   <remap from="/$(arg namespace)/sensors/ntrip/rtcm" to="/$(arg namespace)/mavros/gps_rtk/send_rtcm"/>
+
+#   <!-- <include file="$(find izzyboat_project11)/launch/ntrip.launch">
+#     <arg name="namespace" value="$(arg namespace)"/>
+#   </include> -->
+
+#   <include file="$(find echoboat_project11)/launch/echo.launch">
+#     <arg name="namespace" value="$(arg namespace)"/>
+#     <arg name="platform_name" value="$(arg platform_name)"/>
+#     <arg name="platform_package" value="$(arg platform_package)"/>
+#     <arg name="operator_host" value="$(arg operator_host)"/>
+#     <arg name="fcu_url" value="$(arg fcu_url)"/>
+#     <arg name="gcs_url" value="$(arg gcs_url)"/>
+#   </include> 
+
+#   <group ns="$(arg namespace)">
+
+#     <node name="robot_state_publisher" pkg="robot_state_publisher" type="robot_state_publisher">
+#       <param name="use_tf_static" value="false"/>
+#     </node>
+
+#     <group ns="sensors/cameras/">
+
+#       <node pkg="usb_cam" type="usb_cam_node" name="camera_forward"> 
+#         <param name="camera_name" value="camera_forward" />
+#         <param name="framerate" value="30" />
+#         <param name="image_width" value="1920"/>
+#         <param name="image_height" value="1080"/>
+#         <param name="camera_frame_id" type="string" value="$(arg namespace)/camera_forward_optical" />
+#         <param name="camera_info_url" value="file://$(find izzyboat_project11)/config/camera_forward.yaml"/>
+#       </node>
+
+#       <!-- <node pkg="opencv_dnn" type="opencv_dnn_node" name="yolov8">
+#         <param name="model" value="$(find opencv_dnn)/config/jenna.onnx"/>
+#         <param name="detections_parser" value="opencv_dnn::YOLOv5Parser"/>
+#         <remap from="image" to="camera_forward/image_raw"/>
+#       </node> -->
+
+#       <node pkg="topic_tools" type="throttle" name="throttle_forward" args="messages camera_forward/image_raw/compressed .5"/>
+
+
+#       <include file="$(find depthai_ros_driver)/launch/camera.launch">
+#       </include>
+
+#       <node pkg="topic_tools" type="throttle" name="throttle_oak" args="messages oak/rgb/image_raw/compressed .5"/>
+
+#       <!-- <node pkg="octomap_server" type="octomap_server_node" name="octomap_server" ns="oak">
+#         <remap from="cloud_in" to="points"/>
+#         <param name="resolution" value="0.1"/>
+#         <param name="frame_id" value="$(arg namespace)/map"/>
+#       </node> -->
+
+#     </group>
+
+#     <node pkg="imu_to_tss" type="imu_to_tss.py" name="imu_to_tss">
+#       <remap from="input" to="mavros/imu/data"/>
+#       <param name="address" value="blaze"/>
+#       <param name="port" value="4321"/>
+#     </node> 
+
+#     <group ns="mavros/global_position">
+#       <node pkg="marine_tools" type="navsatfix_to_nmea" name="navsatfix_to_nmea">
+#         <remap from="input" to="raw/fix"/>
+#         <remap from="satellites" to="raw/satellites"/>
+#       </node>
+#       <node pkg="marine_tools" type="nmea_to_udp.py" name="gga_to_udp">
+#         <param name="address" value="blaze"/>
+#       </node>
+#       <node pkg="marine_tools" type="nmea_to_udp.py" name="gga_to_udp_4041">
+#         <param name="address" value="blaze"/>
+#         <param name="port" value="4041"/>
+#       </node>
+#     </group>
+
+#     <group ns="mavros/imu">
+#       <node pkg="marine_tools" type="imu_to_hdt" name="imu_to_hdt">
+#         <remap from="input" to="data"/>
+#       </node>
+#       <node pkg="marine_tools" type="imu_to_pashr" name="imu_to_pashr">
+#         <remap from="input" to="data"/>
+#       </node>
+#       <node pkg="marine_tools" type="nmea_to_udp.py" name="hdt_to_udp">
+#         <param name="address" value="blaze"/>
+#       </node>
+#       <node pkg="marine_tools" type="nmea_to_udp.py" name="hdt_to_udp_4041">
+#         <param name="address" value="blaze"/>
+#         <param name="port" value="4041"/>
+#       </node>
+#       <node pkg="marine_tools" type="nmea_to_udp.py" name="hdt_to_udp_4042">
+#         <param name="address" value="blaze"/>
+#         <param name="port" value="4042"/>
+#       </node>
+#     </group>
+
+#     <group ns="sensors/deltat">
+#       <node pkg="imagenex_deltat" type="deltat.py" name="deltat">
+#         <param name="frame_id" value="$(arg platform_name)/deltat"/>
+#       </node>
+
+#       <node pkg="octomap_server" type="octomap_server_node" name="octomap_server">
+#         <remap from="cloud_in" to="soundings"/>
+#         <param name="resolution" value="0.25"/>
+#         <param name="frame_id" value="$(arg namespace)/map"/>
+#       </node>
+#     </group>
+
+
+
+#     <node pkg="rosbag" type="record" name="logger"  ns="$(arg namespace)" args="-o $(arg logDirectory)/izzyboat --split --duration=15m --repeat-latched --lz4 -a -x  &quot;(.*)image(.*)|(.*)lidar(.*)|(.*)points(.*)|(.*)s57_grids(.*)|(.*)octomap(.*)|(.*)projected_map(.*)|(.*)occupied_cells_vis_array(.*)&quot;
+# "   />
+
+#     <node pkg="rosbag" type="record" name="video_logger"  ns="$(arg namespace)" args="-o $(arg logDirectory)/izzyboat_video --split --duration=15m --repeat-latched --lz4 /izzy/sensors/cameras/camera_forward/camera_info /izzy/sensors/cameras/camera_forward/image_raw/compressed_throttle /izzy/sensors/cameras/oak/rgb/camera_info /izzy/sensors/cameras/oak/rgb/image_raw/compressed_throttle"   />
+
+#     <node pkg="rosbag" type="record" name="sonar_logger"  ns="$(arg namespace)" args="-o $(arg logDirectory)/izzyboat_sonar --split --duration=15m --repeat-latched --lz4 /izzy/sensors/deltat/soundings"   />
+
+
+#     <rosparam command="load" file="$(find izzyboat_project11)/config/izzyboat.yaml"/>
+
+  
+#     <param name="robot_description" command="$(find xacro)/xacro $(arg urdf_model) namespace:=$(arg namespace)"/>
+
+#   </group>
+
+
+# </launch>
---
+++ launch/publish_state_launch.py
@@ -0,0 +1,49 @@
+from ament_index_python.packages import get_package_share_path
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import TextSubstitution
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+def generate_launch_description():
+    namespace = LaunchConfiguration('namespace')
+
+    namespace_arg = DeclareLaunchArgument(
+      "namespace", default_value=TextSubstitution(text="izzy")
+    )
+
+    remappings = [('/tf', 'tf'), ('/tf_static', 'tf_static')]
+    
+    path_to_urdf = get_package_share_path('izzyboat_project11') / 'urdf' / 'izzyboat.urdf'
+    robot_state_publisher_node = Node(
+       package='robot_state_publisher',
+       executable='robot_state_publisher',
+       name='robot_state_publisher',
+       namespace=namespace,
+       parameters=[{
+           'robot_description': ParameterValue(
+            Command(['xacro ', str(path_to_urdf)]), value_type=str
+           )
+       }],
+       remappings=remappings
+    )
+
+
+
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        namespace=namespace,
+        remappings=remappings
+    )
+
+    return LaunchDescription([
+        namespace_arg,
+        robot_state_publisher_node,
+        joint_state_publisher_node
+    ])
+
+
