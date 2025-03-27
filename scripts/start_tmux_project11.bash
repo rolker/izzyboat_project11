82e6bf4968d7c52a7c6357f7fde471db95e9d6f8
Field User <field@ccom.unh.edu>
2025-03-27 13:49:41 -0500

    Start launch with launch gui in virtaul framebuffer and vnc

--- scripts/start_tmux_project11.bash
+++ scripts/start_tmux_project11.bash
@@ -33,10 +33,23 @@ while ! ping -c 1 -W 1 dora; do
     sleep 1
 done
 
+
+
+# start virtual framebuffer
+
+/usr/bin/tmux new -d -s xvfb
+/usr/bin/tmux send-keys "Xvfb :0 -screen 0  1920x1080x24" C-m
+
+/usr/bin/tmux new -d -s fluxbox
+/usr/bin/tmux send-keys "DISPLAY=:0 fluxbox" C-m
+
+/usr/bin/tmux new -d -s x11vnc
+/usr/bin/tmux send-keys "x11vnc -nopw -display :0 -forever" C-m
+
 echo "Wait 5 seconds before launching ROS..."
 sleep 5
 
 /usr/bin/tmux new -d -s project11 
-/usr/bin/tmux send-keys "ros2 launch izzyboat_project11 izzyboat_launch.py logDirectory:=${LOGDIR}" C-m
+/usr/bin/tmux send-keys "DISPLAY=:0 ros2 launch -g izzyboat_project11 izzyboat_launch.py logDirectory:=${LOGDIR}" C-m
 
 } >> "${LOG_FILE}" 2>&1
